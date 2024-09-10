cddd4e6f3b1acde585dadcb088a31ce990bd28c1
ewdijf1234 <jjihye0609@naver.com>
2024-09-10 16:35:26 +0900

    20240910 16:50

---
+++ src/main/java/com/korit/senicare/repository/CareRecordRepository.java
@@ -0,0 +1,13 @@
+package com.korit.senicare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.korit.senicare.entity.CareRecordEntity;
+
+@Repository
+public interface CareRecordRepository extends JpaRepository<CareRecordEntity,Integer> {
+
+
+    
+}
---
+++ src/main/java/com/korit/senicare/repository/TelAuthNumberRepository.java
@@ -0,0 +1,13 @@
+package com.korit.senicare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.stereotype.Repository;
+
+import com.korit.senicare.entity.TelAuthNumberEntity;
+
+@Repository
+// Repository는 interface만 가능!!
+public interface TelAuthNumberRepository extends JpaRepository<TelAuthNumberEntity, String> {
+    
+}
