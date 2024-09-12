109a14f5a15c9bea144156d646b86721edf8593d
ewdijf1234 <jjihye0609@naver.com>
2024-09-12 17:25:03 +0900

    20240912

--- src/main/java/com/korit/senicare/dto/response/ResponseDto.java
+++ src/main/java/com/korit/senicare/dto/response/ResponseDto.java
@@ -40,8 +40,18 @@ public class ResponseDto {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 
+    public static ResponseEntity<ResponseDto> signInFail() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.SIGN_IN_FAIL, ResponseMessage.SIGN_IN_FAIL);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+    }
+
     public static ResponseEntity<ResponseDto> messageSendFail() {
         ResponseDto reponseBody = new ResponseDto(ResponseCode.MESSAGE_SEND_FAIL, ResponseMessage.MESSAGE_SEND_FAIL);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(reponseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> tokenCreateFail() {
+        ResponseDto reponseBody = new ResponseDto(ResponseCode.TOKEN_CREATE_FAIL, ResponseMessage.TOKEN_CREATE_FAIL);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(reponseBody);
     }
 
--- src/main/java/com/korit/senicare/repository/TelAuthNumberRepository.java
+++ src/main/java/com/korit/senicare/repository/TelAuthNumberRepository.java
@@ -1,7 +1,6 @@
 package com.korit.senicare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import org.springframework.stereotype.Repository;
 
 import com.korit.senicare.entity.TelAuthNumberEntity;
